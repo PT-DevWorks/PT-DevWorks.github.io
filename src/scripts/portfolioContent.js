5357c9605c091812bf846f837047f430a28be584
Peter Tran <dev.petertran@gmail.com>
2026-04-03 11:34:26 -0400

    Project Badges
    - Added badges to projects

--- src/scripts/portfolioContent.js
+++ src/scripts/portfolioContent.js
@@ -29,6 +29,33 @@ function filterProjects(filter) {
 
 
 
+/* ===========================================
+   Application/Programming Language: Badge Map
+   =========================================== */
+const normalizeTechnology = (tech) => {
+    const map = {
+        "c#": "csharp",
+        "c++": "cplusplus"
+    };
+
+    const lower = tech.toLowerCase();
+    return map[lower] || lower.replace(/[^a-z0-9]/g, '');
+};
+const technologyBadgeMap  = {
+    "csharp": "/src/assets/images/logos/csharp_Logo.png",
+    "cplusplus": "/src/assets/images/logos/cplusplus_Logo.png",
+    "html": "/src/assets/images/logos/html_Logo.png",
+    "java": "/src/assets/images/logos/java_Logo.png",
+    "python": "/src/assets/images/logos/python_Logo.png",
+    "javaScript": "/src/assets/images/logos/javascript_Logo.png",
+
+    //"unity": "/src/assets/images/logos/unity_Logo.png",
+    //"unreal": "/src/assets/images/logos/unreal_Logo.png",
+    "blender": "/src/assets/images/logos/blender_Logo.png"
+}
+
+
+
 /* =============================================
    Index Page: Portfolio Content Auto Population
    ============================================= */
@@ -49,6 +76,15 @@ fetch('/src/data/portfolioProjects.json')
         // Add tags
         card.dataset.tags = project.projectTags.join(',').toLowerCase();
 
+        // Add badges
+        const technologyBadges = project.developmentTechnologies.map(tech => {
+            const icon = technologyBadgeMap[normalizeTechnology(tech)];
+
+            return icon
+                ? `<img class="techBadge" src="${icon}" alt="${tech}" title="${tech}">`
+                : `<span class="techText">${tech}</span>`;
+        }).join('');
+
         card.innerHTML = `
             <div class="mediaBox">
                 <img src="${project.previewImage}" alt="Project thumbnail" class="previewImage">
@@ -59,7 +95,7 @@ fetch('/src/data/portfolioProjects.json')
             <div class="textBox">
                 <div class="defaultText">
                     <span class="projectTitle">${project.projectTitle}</span>
-                    <span class="projectRole">${project.developmentTechnologies}</span>
+                    <span class="projectBadges">${technologyBadges}</span>
                 </div>
                 <div class="hoverText">Click to learn more</div>
             </div>
